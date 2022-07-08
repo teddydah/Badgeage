3eec21fbe9f7a54a51fee53945202fd582f82f8b
Teddy Dahyot <teddy.dahyot@hotmail.fr>
2022-07-08 12:05:30 +0200

    Création formulaire OrdreFabType.php

--- src/Controller/IlotController.php
+++ src/Controller/IlotController.php
@@ -3,27 +3,31 @@
 namespace App\Controller;
 
 use App\Entity\Ilot;
+use App\Entity\OrdreFab;
+use App\Form\OrdreFabType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/ilot", name="ilot_")
- */
 class IlotController extends AbstractController
 {
     /**
-     * @Route("/{nomAX<\d+>}", name="detail", methods={"GET"})
+     * @Route("/{nomURL}", name="ilot_detail", methods={"GET"})
      */
-    public function detail(Ilot $ilot = null): Response
+    public function detail(Ilot $ilot = null, OrdreFab $ordreFab = null, Request $request): Response
     {
         // ParamConverter => si $ilot est null, alors le contrôleur est exécuté
         if (null === $ilot) {
             throw $this->createNotFoundException('Ilot non trouvé.');
         }
 
+        $form = $this->createForm(OrdreFabType::class, $ordreFab);
+        $form->handleRequest($request);
+
         return $this->render('ilot/detail.html.twig', [
-            'ilot' => $ilot
+            'ilot' => $ilot,
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/OrdreFabType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\OrdreFab;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrdreFabType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('numero', null, [
+                'label' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => OrdreFab::class,
+        ]);
+    }
+}
