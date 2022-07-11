3c85840d028b93303f8f223426ff5dc9c62b1d43
Teddy Dahyot <teddy.dahyot@hotmail.fr>
2022-07-11 10:44:52 +0200

    Màj IlotController.php

--- src/Controller/IlotController.php
+++ src/Controller/IlotController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Badgeage;
+use App\Entity\Client;
 use App\Entity\Ilot;
 use App\Entity\OrdreFab;
 use App\Form\OrdreFabType;
+use App\Repository\OrdreFabRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,17 +17,30 @@ use Symfony\Component\Routing\Annotation\Route;
 class IlotController extends AbstractController
 {
     /**
-     * @Route("/{nomURL}", name="ilot_detail", methods={"GET"})
+     * @Route("/{nomURL}", name="ilot_detail", methods={"GET", "POST"})
      */
-    public function detail(Ilot $ilot = null, OrdreFab $ordreFab = null, Request $request): Response
+    // TODO : renommer méthode
+    public function detail(Ilot $ilot = null, OrdreFab $of = null, Badgeage $badgeage = null, Request $request, EntityManagerInterface $em): Response
     {
         // ParamConverter => si $ilot est null, alors le contrôleur est exécuté
         if (null === $ilot) {
             throw $this->createNotFoundException('Ilot non trouvé.');
         }
 
-        $form = $this->createForm(OrdreFabType::class, $ordreFab);
+        $form = $this->createForm(OrdreFabType::class, $of);
         $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('numero')->getData() == $badgeage->getOrdreFab()->getNumero()) {
+                $this->addFlash('success', $ilot->getNomIRL() . ' : commande ' . $badgeage->getOrdreFab()->getNumero() . ' validée.');
+            } else {
+                $this->addFlash('danger', $ilot->getNomIRL() . ' : l\'OF ' . $form->get('numero')->getData() . ' n\'existe pas.');
+            }
+
+            return $this->redirectToRoute('ilot_detail', [
+                'nomURL' => $ilot->getNomURL()
+            ]);
+        }
 
         return $this->render('ilot/detail.html.twig', [
             'ilot' => $ilot,
