594da5549a1b26fc41209c70cf12b294eecf506c
Teddy Dahyot <teddy.dahyot@hotmail.fr>
2022-07-13 08:55:13 +0200

    Màj fixtures

--- src/DataFixtures/Data/IlotData.php
+++ src/DataFixtures/Data/IlotData.php
@@ -10,42 +10,42 @@ class IlotData
             'nom_irl' => 'Adhésif',
             'nom_url' => 'Adhesif',
             'code_imprimante' => 'ADH',
-            'printer' => 'Printer 1'
+            'printer' => 'Adhésif'
         ],
         [
             'nom_ax' => 30,
             'nom_irl' => 'Annule & Remplace',
             'nom_url' => 'AnnuleATraiter',
             'code_imprimante' => 'ANN',
-            'printer' => 'Printer 2'
+            'printer' => 'Test'
         ],
         [
             'nom_ax' => 160,
             'nom_irl' => 'Débit',
             'nom_url' => 'Debit',
             'code_imprimante' => 'DEB',
-            'printer' => 'Printer 3'
+            'printer' => 'Serrurerie'
         ],
         [
             'nom_ax' => 35,
             'nom_irl' => 'Echantillon',
             'nom_url' => 'Echantillon',
             'code_imprimante' => 'ECH',
-            'printer' => 'Printer 4'
+            'printer' => 'Test'
         ],
         [
             'nom_ax' => 60,
             'nom_irl' => 'Faces Permanente',
             'nom_url' => 'FacesPerm',
             'code_imprimante' => 'FACP',
-            'printer' => 'Printer 5'
+            'printer' => 'Magasin'
         ],
         [
             'nom_ax' => 70,
             'nom_irl' => 'Faces Temporaire',
             'nom_url' => 'FacesTempo',
             'code_imprimante' => 'FACT',
-            'printer' => 'Printer 6'
+            'printer' => 'Magasin'
         ],
 
         [
@@ -53,14 +53,14 @@ class IlotData
             'nom_irl' => 'Fraisage Permanent',
             'nom_url' => 'FraisagePerm',
             'code_imprimante' => 'FRAP',
-            'printer' => 'Printer 7'
+            'printer' => 'Fraisage'
         ],
         [
             'nom_ax' => 50,
             'nom_irl' => 'Fraisage Tempo',
             'nom_url' => 'FraisageTempo',
             'code_imprimante' => 'FRAT',
-            'printer' => 'Printer 8'
+            'printer' => 'Fraisage'
         ],
 
         /* ##### Laquage ##### */
@@ -69,63 +69,63 @@ class IlotData
             'nom_irl' => 'Laquage Accroche',
             'nom_url' => 'LaqAcc',
             'code_imprimante' => 'LAQACC',
-            'printer' => 'Printer 9'
+            'printer' => 'Temporaire'
         ],
         [
             'nom_ax' => 133,
             'nom_irl' => 'Laquage Panneaux',
             'nom_url' => 'LaqPan',
             'code_imprimante' => 'LAQPAN',
-            'printer' => 'Printer 9'
+            'printer' => 'Temporaire'
         ],
         [
             'nom_ax' => 134,
             'nom_irl' => 'Laquage Supports',
             'nom_url' => 'LaqSup',
             'code_imprimante' => 'LAQSUP',
-            'printer' => 'Printer 9'
+            'printer' => 'Temporaire'
         ],
         [
             'nom_ax' => 140,
             'nom_irl' => 'Laquage en sous-traitance',
             'nom_url' => 'LaqSst',
             'code_imprimante' => 'LAQSST',
-            'printer' => 'Printer 9'
+            'printer' => 'Temporaire'
         ],
         [
             'nom_ax' => 9995,
             'nom_irl' => 'Peinture',
             'nom_url' => 'Peinture',
             'code_imprimante' => 'MOB',
-            'printer' => 'Printer 9'
+            'printer' => 'Temporaire'
         ],
         [
             'nom_ax' => 9996,
             'nom_irl' => 'Laquage',
             'nom_url' => 'Laquage',
             'code_imprimante' => 'LAQ',
-            'printer' => 'Printer 9'
+            'printer' => 'Laquage'
         ],
         [
             'nom_ax' => 9997,
             'nom_irl' => 'Etiquettes Laquage : RAL',
             'nom_url' => 'Laquage', // LaqImpr ImprEtiqLaqRAL LaqEtiqRAL
             'code_imprimante' => 'LAQRAL',
-            'printer' => 'Printer 9'
+            'printer' => 'Laquage'
         ],
         [
             'nom_ax' => 9998,
             'nom_irl' => 'Etiquettes Laquage : OF',
             'nom_url' => 'Laquage', // LaqEtiqOF
             'code_imprimante' => 'LAQOF',
-            'printer' => 'Printer 9'
+            'printer' => 'Laquage'
         ],
         [
             'nom_ax' => 9999,
             'nom_irl' => 'Etiquettes Laquage',
             'nom_url' => 'LaqEtiqHome',
             'code_imprimante' => 'LAQIMPR',
-            'printer' => 'Printer 9'
+            'printer' => 'Test'
         ],
         /* ##### Laquage ##### */
 
@@ -134,70 +134,70 @@ class IlotData
             'nom_irl' => 'Magasin',
             'nom_url' => 'Magasin',
             'code_imprimante' => 'MAG',
-            'printer' => 'Printer 10'
+            'printer' => 'Magasin'
         ],
         [
             'nom_ax' => 40,
             'nom_irl' => 'Mise en Fabrication',
             'nom_url' => 'MiseEnFab',
             'code_imprimante' => 'MEF',
-            'printer' => 'Printer 11'
+            'printer' => 'Chef Atelier'
         ],
         [
             'nom_ax' => 110,
             'nom_irl' => 'Mobilier Urbain',
             'nom_url' => 'MobilierUrbain',
             'code_imprimante' => 'MOBU',
-            'printer' => 'Printer 12'
+            'printer' => 'Temporaire'
         ],
         [
             'nom_ax' => 10,
             'nom_irl' => 'Création de l\'OF',
             'nom_url' => 'OFATraiter',
             'code_imprimante' => 'OFT',
-            'printer' => 'Printer 13'
+            'printer' => 'Test'
         ],
         [
             'nom_ax' => 90,
             'nom_irl' => 'Permanente',
             'nom_url' => 'Permanente',
             'code_imprimante' => 'PERM',
-            'printer' => 'Printer 14'
+            'printer' => 'Magasin'
         ],
         [
             'nom_ax' => 180,
             'nom_irl' => 'Pliage',
             'nom_url' => 'Pliage',
             'code_imprimante' => 'PLI',
-            'printer' => 'Printer 15'
+            'printer' => 'Temporaire'
         ],
         [
             'nom_ax' => 230,
             'nom_irl' => 'Prépa Permanente',
             'nom_url' => 'PrepaPerm',
             'code_imprimante' => 'PPER',
-            'printer' => 'Printer 16'
+            'printer' => 'Magasin'
         ],
         [
             'nom_ax' => 20,
             'nom_irl' => 'SAV',
             'nom_url' => 'SAVATraiter',
             'code_imprimante' => 'SAV',
-            'printer' => 'Printer 17'
+            'printer' => 'Test'
         ],
         [
             'nom_ax' => 170,
             'nom_irl' => 'Serrurerie',
             'nom_url' => 'Serrurerie',
             'code_imprimante' => 'SERR',
-            'printer' => 'Printer 18'
+            'printer' => 'Serrurerie'
         ],
         [
             'nom_ax' => 80,
             'nom_irl' => 'Temporaire',
             'nom_url' => 'Temporaire',
             'code_imprimante' => 'TEMP',
-            'printer' => 'Printer 19'
+            'printer' => 'Magasin'
         ]
     ];
 }
--- src/DataFixtures/Data/PrinterData.php
+++ src/DataFixtures/Data/PrinterData.php
@@ -6,8 +6,36 @@ class PrinterData
 {
     public static $printerData = [
         [
+            'nom' => 'Adhésif',
+            'ip' => '192.168.35.202'
+        ],
+        [
+            'nom' => 'Temporaire',
+            'ip' => '192.168.35.208'
+        ],
+        [
+            'nom' => 'Serrurerie',
+            'ip' => '192.168.35.206'
+        ],
+        [
             'nom' => 'Fraisage',
-            'ip' => '192.168.35.201'
+            'ip' => '192.168.35.47'
+        ],
+        [
+            'nom' => 'Laquage',
+            'ip' => '192.168.35.209'
+        ],
+        [
+            'nom' => 'Magasin',
+            'ip' => '192.168.35.205'
+        ],
+        [
+            'nom' => 'Chef Atelier',
+            'ip' => '192.168.35.207'
+        ],
+        [
+            'nom' => 'Test',
+            'ip' => '192.168.34.149'
         ]
     ];
 }
