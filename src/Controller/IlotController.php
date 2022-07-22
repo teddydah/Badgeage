ae6c478aa02092e9e42a22bfbe613c6c850029e7
Teddy Dahyot <teddy.dahyot@hotmail.fr>
2022-07-22 22:41:03 +0200

    Màj IlotController.php

--- src/Controller/IlotController.php
+++ src/Controller/IlotController.php
@@ -17,37 +17,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route(name="ilot_")
- */
 class IlotController extends AbstractController
 {
-    /**
-     * @Route("/{nomURL}", name="of", methods={"GET", "POST"})
-     */
-    // TODO : renommer
-    public function getOF(): Response
-    {
-        return $this->render('view.html.twig', [
 
-        ]);
-    }
-
-    /**
-     * @Route("Peinture/{nomURL}", name="paint", methods={"GET"})
-     */
-    public function paint(Ilot $ilot = null): Response
-    {
-        if (null === $ilot) {
-            throw $this->createNotFoundException('Ilot non trouvé.');
-        }
-
-        $form = $this->createForm(OrdreFabType::class, $ordreFab);
-        $form->handleRequest($request);
-
-        return $this->render('ilot/paint.html.twig', [
-            'ilot' => $ilot,
-            'form' => $form->createView()
-        ]);
-    }
 }
