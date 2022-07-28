c7e4c5a7063f1e041b9345550b67cfb14cfa99d1
Teddy Dahyot <teddy.dahyot@hotmail.fr>
2022-07-28 12:30:36 +0200

    Màj fixtures

--- src/DataFixtures/Data/UserData.php
+++ src/DataFixtures/Data/UserData.php
@@ -7,6 +7,11 @@ class UserData
     public static array $userData = [
         [
             'email' => 'renaud.l@selfsignal.fr',
+            'roles' => 'ROLE_SUPER_ADMIN',
+            'password' => '$2y$13$gydYwHQ0igUJxYZTiRwIQ.Vi3.S8xLSt6tMY7H8IOVxrfQVCmLKxq'
+        ],
+        [
+            'email' => 'ellylldhan@protonmail.com',
             'roles' => 'ROLE_ADMIN',
             'password' => '$2y$13$gydYwHQ0igUJxYZTiRwIQ.Vi3.S8xLSt6tMY7H8IOVxrfQVCmLKxq'
         ]
