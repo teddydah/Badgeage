84b826a9382714626edce53ce921ef8bf2f92f5a
Teddy Dahyot <teddy.dahyot@hotmail.fr>
2022-07-28 08:59:04 +0200

    Màj lien page précédente

--- src/Service/PreviousPage.php
+++ src/Service/PreviousPage.php
@@ -6,10 +6,10 @@ class PreviousPage
 {
     public function pagePrecedente(): string
     {
-        // Home
+        // URL de la page d'accueil
         define("HOME", "http://localhost/badgeage/public/");
 
-        // nomURL
+        // nomURL => îlots
         define('NOM_URL', [
             'Adhesif',
             'Debit',
@@ -27,13 +27,18 @@ class PreviousPage
             'Temporaire'
         ]);
 
+        // URL page courante
         $url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
         $uri = $url;
 
         foreach (NOM_URL as $nomURL) {
+            // Si l'URL correspond à celle de la page "http://localhost/badgeage/public/badgeage/$nomURL/view" :
+            // $uri = URL page d'accueil
             if ($url == HOME . "badgeage/" . $nomURL . "/view") {
                 $uri = HOME;
+                // Sinon si l'URL correspond à celle de la page "$nomURL/impression" :
+                // $uri = URL page "http://localhost/badgeage/public/badgeage/$nomURL/view"
             } else if ($url == HOME . $nomURL . "/impression") {
                 $uri = HOME . "badgeage/" . $nomURL . "/view";
             }
