84b9634a6fb4f40ab2ff8fdd8a383352715a6ca9
Teddy Dahyot <teddy.dahyot@hotmail.fr>
2022-08-16 11:55:17 +0200

    Màj EntityTest.php

--- tests/Unit/EntityTest.php
+++ tests/Unit/EntityTest.php
@@ -12,7 +12,7 @@ class EntityTest extends KernelTestCase
     {
         // Test de l'entité Printer
         return (new Printer())
-            ->setNom('printer')
+            ->setNom('Printer')
             ->setIp('192.168.35.000')
             ->setPort(9100);
     }
@@ -23,27 +23,34 @@ class EntityTest extends KernelTestCase
         $errors = self::$container->get('validator')->validate($printer);
         $messages = [];
 
-        /**
-         * @var ConstraintViolation $error
-         */
+        /** @var ConstraintViolation $error */
         foreach ($errors as $error) {
             $messages[] = $error->getPropertyPath() . ' => ' . $error->getMessage();
         }
         $this->assertCount($number, $errors, implode(', ', $messages));
     }
 
+    // Test la validité de l'entité Printer
     public function testValidEntity()
     {
-        $this->assertHasErrors($this->getEntity(), 0);
+        $this->assertHasErrors($this->getEntity());
     }
 
-    public function testInvalidBlankNomEntity()
+    // Test le remplissage de la propriété "nom" => vide ?
+    public function testInvalidBlankNom()
     {
         $this->assertHasErrors($this->getEntity()->setNom(''), 1);
     }
 
-    public function testInvalidBlankIpEntity()
+    // Test le remplissage de la propriété "ip" => vide ?
+    public function testInvalidBlankIp()
     {
         $this->assertHasErrors($this->getEntity()->setIp(''), 1);
+    }
+
+    // Test l'unicité de la propriété "nom" => déjà utilisé ?
+    public function testInvalidUsedNom()
+    {
+        $this->assertHasErrors($this->getEntity()->setNom('Chef Atelier'), 1);
     }
 }
