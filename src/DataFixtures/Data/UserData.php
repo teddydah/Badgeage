b2b5f45080c5e7a0fe0dd11421520d06ed2c21c3
Teddy Dahyot <teddy.dahyot@hotmail.fr>
2024-02-27 16:34:50 +0100

    Màj projet Badgeage

--- src/DataFixtures/Data/UserData.php
+++ src/DataFixtures/Data/UserData.php
@@ -6,12 +6,12 @@ class UserData
 {
     public static array $userData = [
         [
-            'email' => 'renaud.l@selfsignal.fr',
+            'email' => 'john.doe@gmail.com',
             'roles' => 'ROLE_ADMIN',
             'password' => '$2y$13$gydYwHQ0igUJxYZTiRwIQ.Vi3.S8xLSt6tMY7H8IOVxrfQVCmLKxq'
         ],
         [
-            'email' => 'ellylldhan@protonmail.com',
+            'email' => 'john.doe@live.fr',
             'roles' => 'ROLE_USER',
             'password' => '$2y$13$gydYwHQ0igUJxYZTiRwIQ.Vi3.S8xLSt6tMY7H8IOVxrfQVCmLKxq'
         ]
